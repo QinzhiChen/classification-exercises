ebfed9e80525d6104755f26bfa98d73779d3af22
QinzhiChen <qinzhic@gmail.com>
2022-08-11 13:41:20 -0500

    files modified

--- prepare.py
+++ prepare.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[2]:
 
 
 # prep exercise
@@ -10,20 +10,8 @@ from sklearn.impute import SimpleImputer
 import acquire
 import warnings
 import pandas as pd
-df_iris=acquire.get_iris_data()
-
-# In[2]:
 
 
-df_iris.drop(columns=['species_id','measurement_id','Unnamed: 0'],inplace=True)
-
-
-
-df_iris.rename(columns={'species_name':'species'},inplace=True)
-
-
-dummy_df_iris = pd.get_dummies(df_iris.species, dummy_na=False, drop_first=[True])
-df_iris=pd.concat([df_iris,dummy_df_iris],axis=1)
 
 
 # In[ ]:
@@ -38,6 +26,15 @@ def prep_iris(df):
     return df_iris
 
 
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
 def clean_iris(df):
     df_iris=prep_iris(df)
     train, test = train_test_split(df_iris, test_size=.2, 
@@ -46,6 +43,11 @@ def clean_iris(df):
                  random_state=123, stratify=train.versicolor)
     
     return n_train, validate, test
+
+
+# In[ ]:
+
+
 
 
 
@@ -91,35 +93,36 @@ def prep_titanic(df):
 def split_titanic(df):
     df_titanic=prep_titanic(df)
     train, test = train_test_split(df_titanic, test_size=.2, 
-                               random_state=123, stratify=df_titanic[target])
+                               random_state=123, stratify=df_titanic['survived'])
     n_train, validate = train_test_split(train, test_size=.25, 
-                 random_state=123, stratify=train.target)
+                 random_state=123, stratify=train.survived)
     
     return n_train, validate, test
 
 
-# In[ ]:
+# In[1]:
 
 
 prep_titanic(df_titanic)
 
 
-# In[ ]:
+# In[3]:
 
 
 df_telco=acquire.get_telco_data()
 
 
-# In[ ]:
+# In[7]:
 
 
-df_telco.drop(columns=['Unnamed: 0','internet_service_type_id','phone_service.1','multiple_lines.1','internet_service_type_id.1','online_security.1','online_backup.1','device_protection.1','contract_type_id','payment_type_id','tech_support.1','streaming_tv.1','streaming_movies.1','contract_type_id.1' ,'paperless_billing.1','payment_type_id.1','monthly_charges.1','total_charges.1','internet_service_type_id.2'],inplace=True)
+df_telco.drop(columns=['internet_service_type_id','contract_type_id','payment_type_id',
+                       ],inplace=True)
 
 
-# In[ ]:
+# In[6]:
 
 
-df_telco
+
 
 
 # In[ ]:
@@ -135,12 +138,13 @@ df_telco=pd.concat([df_telco ,dummy_df_telco],axis=1)
 df_telco.drop(columns=['partner','dependents','phone_service','multiple_lines','online_security','online_backup','device_protection','tech_support','streaming_tv','streaming_movies','paperless_billing','churn','internet_service_type'],inplace=True)
 
 
-# In[ ]:
+# In[8]:
 
 
 def prep_telco(df):
     df_telco=acquire.get_telco_data()
-    df_telco.drop(columns=['Unnamed: 0','internet_service_type_id','phone_service.1','multiple_lines.1','internet_service_type_id.1','online_security.1','online_backup.1','device_protection.1','contract_type_id','payment_type_id','tech_support.1','streaming_tv.1','streaming_movies.1','contract_type_id.1' ,'paperless_billing.1','payment_type_id.1','monthly_charges.1','total_charges.1','internet_service_type_id.2'],inplace=True)
+    df_telco.drop(columns=['internet_service_type_id','contract_type_id','payment_type_id',
+                       ],inplace=True)
     dummy_df_telco = pd.get_dummies(df_telco[['partner','dependents','phone_service','multiple_lines','online_security','online_backup','device_protection','tech_support','streaming_tv','streaming_movies','paperless_billing','churn','internet_service_type']], dummy_na=False, drop_first=[True])
     df_telco=pd.concat([df_telco ,dummy_df_telco],axis=1)
     df_telco.drop(columns=['partner','dependents','phone_service','multiple_lines','online_security','online_backup','device_protection','tech_support','streaming_tv','streaming_movies','paperless_billing','churn','internet_service_type'],inplace=True)
@@ -156,9 +160,9 @@ def prep_telco(df):
 # In[ ]:
 
 
-def split_telco(pre_telco):
-    prep_telco=prep_telco(df)
-    n_train, test = train_test_split(prep_telco, test_size=.2, 
+def split_telco(df):
+    pr_telco=prep_telco(df)
+    n_train, test = train_test_split(pr_telco, test_size=.2, 
                                random_state=123)
     train, validate = train_test_split(n_train, test_size=.25, 
                  random_state=123)
